e6ad7905aa61b66741cb1583dc10a812ff3b7f29
Sergey Mironov <66295155+FeagurSE@users.noreply.github.com>
2020-06-18 09:44:27 +0300

    Add files via upload

---
+++ Otchetnost.php
@@ -0,0 +1,56 @@
+			<div id="account-pp" class="container">
+				<section class="links">
+					[[*Отчётность 1 Секция Оглавление]]
+				</section>
+			</div>
+
+			<div id="F-1" class="overlay">
+				<div class="popup">
+					[[*Отчётность 1 Секция 1 Наполнение]]
+				</div>
+			</div>
+
+			<div id="F-2" class="overlay">
+				<div class="popup">
+					[[*Отчётность 1 Секция 2 Наполнение]]
+				</div>
+			</div>
+
+			<div id="F-3" class="overlay">
+				<div class="popup">
+					[[*Отчётность 1 Секция 3 Наполнение]]
+				</div>
+			</div>
+
+			<div id="F-4" class="overlay">
+				<div class="popup">
+					[[*Отчётность 1 Секция 4 Наполнение]]
+				</div>
+			</div>
+
+			<div id="F-5" class="overlay">
+				<div class="popup">
+					[[*Отчётность 1 Секция 5 Наполнение]]
+				</div>
+			</div>
+
+			<div id="Audit" class="overlay">
+				<div class="popup">
+					[[*Отчётность 1 Секция 6 Наполнение]]
+				</div>
+			</div>
+
+			<div id="Invest" class="overlay">
+				<div class="popup">
+					[[*Отчётность 1 Секция 7 Наполнение]]
+				</div>
+			</div>
+
+			<div id="Structure" class="overlay">
+				<div class="popup">
+					[[*Отчётность 1 Секция 8 Наполнение]]
+				</div>
+			</div>
+
+		</div>
+	</div>
